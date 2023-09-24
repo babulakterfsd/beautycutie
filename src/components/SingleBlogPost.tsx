e5a11c1eec07b05046ba59c23e42245e0c34c2d3
Babul Akter <babulakterfsd@gmail.com>
2023-09-25 02:25:51 +0600

    single blog post ongoing

--- src/components/SingleBlogPost.tsx
+++ src/components/SingleBlogPost.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import arrowupright from '../../public/assets/images/arrow-up-right.png';
 import author from '../../public/assets/images/author.png';
 import fb from '../../public/assets/images/fb.png';
 import insta from '../../public/assets/images/insta.png';
@@ -149,6 +150,75 @@ const SingleBlogPost = () => {
         </div>
       </div>
 
+      {/* recent posts */}
+      <div className="py-16 lg:py-24 bg-gray-50 mt-12">
+        <div className="main-container">
+          {/* header */}
+          <div className="header flex justify-between items-start">
+            <div className="textcontent">
+              <h5 className="text-primary font-inter font-medium text-sm md:text-[1rem]">
+                Our blog
+              </h5>
+              <h6 className="text-cyan font-medium md:font-semibold text-3xl lg:text-4xl lg:leading-[44px] lg:tracking-[-.72px] font-inter mt-2">
+                Lastest blog posts
+              </h6>
+              <p className="text-cyan mt-3 font-inter">
+                All useful information for beauty industry
+              </p>
+            </div>
+            <Link href="/blog">
+              <button className="bg-primary text-white font-inter font-normal py-3 px-5 rounded focus:outline-none focus:shadow-outline mt-2">
+                View all posts
+              </button>
+            </Link>
+          </div>
+          {/* posts */}
+          <div className="grid grid-cols-12 md:gap-x-16 gap-y-16 mt-16">
+            {RecentPostData.map((rpost) => {
+              return (
+                <div
+                  className="col-span-12 md:col-span-6 lg:col-span-4 cursor-pointer"
+                  key={rpost.id}
+                >
+                  <Image
+                    src={rpost.Image}
+                    alt="recent"
+                    width={300}
+                    height={300}
+                    className="mb-7"
+                  />
+                  <p className="text-[#3acce9] mb-3 font-roboto">{`${rpost.type}`}</p>
+                  <div className="flex gap-x-7 items-center mb-3">
+                    <h4 className="text-cyan font-inter font-medium md:font-semibold text-xl">
+                      {`${rpost.title}`}
+                    </h4>
+                    <Image src={arrowupright} alt="link" />
+                  </div>
+                  <p className="text-cyan font-inter mb-4">{`${rpost.content}`}</p>
+                  <div className="flex flex-row gap-x-5 mt-3">
+                    <Image
+                      src={rpost.author.image}
+                      alt="author"
+                      className="self-start object-contain rounded-full"
+                      width={30}
+                      height={30}
+                    />
+                    <div className="flex flex-col">
+                      <h5 className="text-cyan font-medium md:font-semibold font-inter">
+                        {`${rpost.author.name}`}
+                      </h5>
+                      <p className="text-cyan text-sm font-inter">
+                        {`${rpost.author.date}`}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
       {/* newsletter */}
       <div className="px-4 md:px-0">
         <NewsletterFull />
@@ -158,3 +228,45 @@ const SingleBlogPost = () => {
 };
 
 export default SingleBlogPost;
+
+const RecentPostData = [
+  {
+    id: 1,
+    Image: 'https://i.ibb.co/zPz7T6z/rpost1.png',
+    title: 'New anti-ageing massage ',
+    type: 'skin',
+    content:
+      'How do you create compelling presentations that wow your colleagues and impress your managers?',
+    author: {
+      name: 'Olivia Rhye',
+      image: 'https://i.ibb.co/wcth7wT/author.png',
+      date: 'May 20, 2021',
+    },
+  },
+  {
+    id: 2,
+    Image: 'https://i.ibb.co/xFSnPPm/rpost2.png',
+    title: 'How to look after your hair ',
+    type: 'hair',
+    content:
+      'How do you create compelling presentations that wow your colleagues and impress your managers?',
+    author: {
+      name: 'Olivia Rhye',
+      image: 'https://i.ibb.co/wcth7wT/author.png',
+      date: 'May 20, 2021',
+    },
+  },
+  {
+    id: 3,
+    Image: 'https://i.ibb.co/smfJ3ry/rpost3.png',
+    title: 'New product for your skin ',
+    type: 'products',
+    content:
+      'How do you create compelling presentations that wow your colleagues and impress your managers?',
+    author: {
+      name: 'Olivia Rhye',
+      image: 'https://i.ibb.co/wcth7wT/author.png',
+      date: 'May 20, 2021',
+    },
+  },
+];
