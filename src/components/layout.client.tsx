da05791713df12f8e2eab32c088ed7df5082202e
Babul Akter <babulakterfsd@gmail.com>
2023-09-25 15:23:56 +0600

    v 1.0.0 completed

--- src/components/layout.client.tsx
+++ src/components/layout.client.tsx
@@ -1,14 +1,42 @@
 'use client';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  const [windowHeight, setWindowHeight] = useState(false);
+
   useEffect(() => {
     Aos.init();
   }, []);
 
-  return children;
+  useEffect(() => {
+    const handleScroll = () => {
+      setWindowHeight(window.scrollY > 500);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <>
+      {/* scroll to bottom */}
+      {!windowHeight && (
+        <button type="button" className="scrollToDown cursor-default" />
+      )}
+      {/* scroll to top */}
+      {windowHeight && (
+        <button
+          type="button"
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+          }}
+          className="scrollToTop"
+        />
+      )}
+      {children}
+    </>
+  );
 };
 
 export default ClientLayout;
